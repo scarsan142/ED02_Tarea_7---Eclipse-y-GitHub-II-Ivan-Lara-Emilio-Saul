af8a7c8e9952f0ef53707614a879c432802e18e5
Saúl Carbajosa <scarsan142@g.educaand.es>
2022-12-12 19:05:55 +0100

    Prog1_0

--- src/Prog.java
+++ src/Prog.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio=leerradio.nextInt();
+		}
+		}
