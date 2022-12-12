09592e6301bcdcdd606f75905129fcff15c0fa5d
Saúl Carbajosa <scarsan142@g.educaand.es>
2022-12-12 19:00:29 +0100

    Creación de el codigo de la rama prog 2

--- src/Prog.java
+++ src/Prog.java
@@ -3,7 +3,12 @@ public class Prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	double area;
+	int radio=0;
+area = (Math.PI) * (Math.pow(radio, 2));
+System.out.println("El área de la esfera es "+area);
+System.out.println("Finalizado Version 2");
+	
 	}
 
 }
