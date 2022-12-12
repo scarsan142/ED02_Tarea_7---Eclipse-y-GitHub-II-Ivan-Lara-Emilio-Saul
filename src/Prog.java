6d37dbbf48b3c6278c0f13e633b0418a3f292281
Saúl Carbajosa <scarsan142@g.educaand.es>
2022-12-12 19:13:32 +0100

    Terminado el codigo de la rama 1

--- src/Prog.java
+++ src/Prog.java
@@ -6,8 +6,9 @@ public class Prog {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner leerradio = new Scanner(System.in);
-		int radio;
+		int radio=0;
 		double volumen;
+		volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
 		System.out.println("Introduce el radio de la esfera");
 		radio=leerradio.nextInt();
 		}
